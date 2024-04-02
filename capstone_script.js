44cee677048fad3721a8c1f042946b8898b1649a
[Chris Smith] <[csmith3723@gmail.com]>
2024-04-01 21:44:21 -0400

    Labels added, neighborhood object in js, and css updated

---
+++ capstone_script.js
@@ -0,0 +1,67 @@
+const neighborhoods = [
+  {
+    name: "Apollo Beach",
+    oneBrPrice: 1525,
+    twoBrPrice: 1895,
+    housePrice: 450000,
+  },
+
+  {
+    name: "Brandon",
+    oneBrPrice: 1605,
+    twoBrPrice: 1800,
+    housePrice: 350000,
+  },
+
+  {
+    name: "Carrollwood",
+    oneBrPrice: 1638,
+    twoBrPrice: 1960,
+    housePrice: 365000,
+  },
+
+  {
+    name: "Citrus Park",
+    oneBrPrice: 1314,
+    twoBrPrice: 1900,
+    housePrice: 385000,
+  },
+  {
+    name: "Lutz",
+    oneBrPrice: 1673,
+    twoBrPrice: 1982,
+    housePrice: 485000,
+  },
+  {
+    name: "Northdale",
+    oneBrPrice: 1815,
+    twoBrPrice: 2040,
+    housePrice: 425000,
+  },
+  {
+    name: "South Tampa",
+    oneBrPrice: 1700,
+    twoBrPrice: 2200,
+    housePrice: 630000,
+  },
+  {
+    name: "Temple Terrace",
+    oneBrPrice: 1500,
+    twoBrPrice: 1675,
+    housePrice: 300000,
+  },
+
+  {
+    name: "Westchase",
+    oneBrPrice: 1856,
+    twoBrPrice: 2337,
+    housePrice: 455000,
+  },
+
+  {
+    name: "Wesley Chapel",
+    oneBrPrice: 1650,
+    twoBrPrice: 1885,
+    housePrice: 385000,
+  },
+];
